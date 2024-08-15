c33b9ae5c92de8fed9d31c881b899cb016608654
Pius Aaron <piuschbz@gmail.com>
2024-08-15 11:39:42 +0100

    Task 1 check check

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
-"""Auth App Db class definition.
+"""DB module
 """
-
-from bcrypt import hashpw
 from sqlalchemy import create_engine
-from sqlalchemy.exc import NoResultFound
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from typing import TypeVar
-from user import User
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
-from user import Base
+from user import Base, User
 
 
 class DB:
@@ -36,37 +33,39 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Adds user data to db.
         """
-
+        add new user
+        Return:
+            a new user
+        """
         new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
-        self._session.refresh(new_user)
-
+        session = self._session
+        session.add(new_user)
+        session.commit()
+        session.refresh(new_user)
         return new_user
 
-    def find_user_by(self, **kwargs):
-        """Finds users with matching attributes in kwargs.
-        Return
-          - list of user instances matching kwargs
-        Exceptions
-          - Raises NoResultFound if no matching values
-          - InvalidRequestError when wrong query arguments are passed
+    def find_user_by(self, **kwargs) -> User:
         """
-
-        user = self._session.query(User).filter_by(**kwargs).first()
-
+        find user and raise error if not found
+        or invalid
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+        except Exception as e:
+            raise InvalidRequestError
         if not user:
             raise NoResultFound
-
         return user
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        """Updates user data"""
-
+        """
+        find and update a user
+        """
         user = self.find_user_by(id=user_id)
-        for k, v in kwargs.items():
-            if k not in user.__table__.columns.keys():
+        for key, value in kwargs.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
+            else:
                 raise ValueError
-            setattr(user, k, v)
+        self._session.commit()
