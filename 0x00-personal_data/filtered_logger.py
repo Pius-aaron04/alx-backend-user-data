f92c64e914a75b06ab56e55ea5e0d8543d4fb4df
Pius Aaron <piuschbz@gmail.com>
2024-08-16 17:59:29 +0100

    Created a stream logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,21 @@
 from typing import List
 import re
 import logging
+import csv
+
+PII_FIELDS = ("password", "ssn", "phone", "ip", "last_login")
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """Obfuscate specified fields data"""
+
+    return_val = re.sub(r"(\w+)=([a-zA-Z0-9@\.\-\(\)\ \:\^\<\>\~\$\%" +
+                        r"\@\?\!\/]*)",
+                        lambda match_: match_.group(1) + '=' + redaction
+                        if match_.group(1) in fields
+                        else match_.group(0), message)
+    return return_val
 
 
 class RedactingFormatter(logging.Formatter):
@@ -12,23 +27,23 @@ class RedactingFormatter(logging.Formatter):
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
+    SEPARATOR = "; "
 
-    def __init__(self , fields):
+    def __init__(self, fields):
         """COnstructor method."""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """Format log record."""
-        return filter_datum(self.fields, self.REDACTION, super(RedactingFormatter, self).format(record), self.SEPARATOR)
+        return filter_datum(self.fields, self.REDACTION,
+                            super(RedactingFormatter, self).format(record),
+                            self.SEPARATOR)
 
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-    """Obfuscate specified fields data"""
 
-    return_val = re.sub(r'(\w+)=[a-zA-Z0-9@.-\:]*',
-                        lambda match_: match_.group(1) + '=' + redaction
-                        if match_.group(1) in fields
-                        else match_.group(0), message)
-    return return_val
+def get_logger() -> logging.Logger:
+    """returns a logger object."""
+    logger = logging.getLogger(__name__)
+    logger.addHandler(logging.StreamHandler())
+    logger.setFormatter(RedactingFormatter)
+    return logger
